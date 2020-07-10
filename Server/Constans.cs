ae69f630cd0af9bb058b3719c023a0af4320df5c
diymine <diymine@163.com>
2020-07-11 00:05:47 +0800

    Jwt study

---
+++ Server/Constans.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constans
+    {
+        public const string Issuer = Audiance;
+        public const string Audiance = "https://localhost:44330/";
+        public const string Secret = "not_too_short_othewise_might_errors";
+    }
+}
